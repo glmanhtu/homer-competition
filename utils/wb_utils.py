e029cfa49f9c6709c70fa8253b8d05a69bbd8b91
Manh Tu VU <glmanhtu@gmail.com>
2023-03-07 15:50:27 +0100

    Fix minor issue

--- utils/wb_utils.py
+++ utils/wb_utils.py
@@ -12,6 +12,7 @@ def bounding_boxes(tensor_img, v_boxes, v_labels, v_scores, box_scale_pred, log_
     raw_image = torchvision.transforms.ToPILImage()(tensor_img)
     raw_image.thumbnail((log_width, log_height), Image.ANTIALIAS)
     all_boxes = []
+    print(v_labels)
     # plot each bounding box for this image
     for b_i, box in enumerate(v_boxes):
         # get coordinates and labels
@@ -22,7 +23,7 @@ def bounding_boxes(tensor_img, v_boxes, v_labels, v_scores, box_scale_pred, log_
                 "minY": box[1],
                 "maxY": box[3]
             },
-            "class_id": v_labels[b_i],
+            "class_id": int(v_labels[b_i]),
             # optionally caption each box with its class and score
             "box_caption": "%s (%.3f) - Scale: (%.3f)" % (class_id_to_label[v_labels[b_i]], v_scores[b_i],
                                                           box_scale_pred[b_i]),
