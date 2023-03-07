c7f3bc2793d69e9b6e4e4db7b02d1725bffea301
Manh Tu VU <glmanhtu@gmail.com>
2023-03-07 15:35:41 +0100

    Fix minor issue

--- utils/wb_utils.py
+++ utils/wb_utils.py
@@ -24,7 +24,8 @@ def bounding_boxes(tensor_img, v_boxes, v_labels, v_scores, box_scale_pred, log_
             },
             "class_id": class_id_to_label[v_labels[b_i]],
             # optionally caption each box with its class and score
-            "box_caption": "%s (%.3f) - Scale: (%.3f)" % (class_id_to_label[b_i], v_scores[b_i], box_scale_pred[b_i]),
+            "box_caption": "%s (%.3f) - Scale: (%.3f)" % (class_id_to_label[v_labels[b_i]], v_scores[b_i],
+                                                          box_scale_pred[b_i]),
             "domain": "pixel",
             "scores": {"score": v_scores[b_i]}}
         all_boxes.append(box_data)
