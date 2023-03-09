f9fe2f262d824c8cd6320e39b02b7ad483186567
Manh Tu VU <glmanhtu@gmail.com>
2023-03-09 12:48:16 +0100

    Update loss func

--- model/model_wrapper.py
+++ model/model_wrapper.py
@@ -122,7 +122,7 @@ class ModelWrapper:
 
         loss_extra_head = all_losses['loss_extra_head']
         del all_losses['loss_extra_head']
-        detection_loss = sum(all_losses.values())
+        detection_loss = sum(all_losses.values()) / len(all_losses)
         return (2 * detection_loss + loss_extra_head) / 3
 
     def optimise_params(self, loss):
