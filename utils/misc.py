ee6e1dd9b37c0a4cabb3363c779c72fe8bc5a706
Manh Tu VU <glmanhtu@gmail.com>
2023-03-11 12:58:37 +0100

    Update

--- utils/misc.py
+++ utils/misc.py
@@ -77,14 +77,14 @@ def flatten(l):
     return [item for sublist in l for item in sublist]
 
 
-def estimate_letter_scale_performance(v_boxes, letter_boxes, v_scales):
+def estimate_letter_scale_performance(v_boxes, letter_boxes, v_scales, ref_scale=0.0786):
     pred, gt = [], []
     for b_i, box in enumerate(v_boxes):
         # get coordinates and labels
         l_boxes = filter_boxes(box, letter_boxes)
         if len(l_boxes) > 0:
             gt_box_height = (l_boxes[:, 3] - l_boxes[:, 1]).mean()
-            gt.append(gt_box_height)
+            gt.append(gt_box_height / ref_scale)
             pred_box_height = v_scales[b_i] * (box[3] - box[1])
             pred.append(pred_box_height)
 
