c758c786f9090eee67b662148c759ba49fe798f1
Manh Tu VU <glmanhtu@gmail.com>
2023-03-15 18:00:33 +0100

    Update network

--- model/letter_detection_rcnn.py
+++ model/letter_detection_rcnn.py
@@ -13,20 +13,20 @@ class LetterDetectionRCNN(nn.Module):
                                                                                min_size=img_size,
                                                                                max_size=img_size)
 
-        anchor_sizes = (
-                           (
-                               16,
-                               32,
-                               64,
-                               96,
-                               128,
-                           ),
-                       ) * 3
-        aspect_ratios = ((0.75, 1.0, 1.25),) * len(anchor_sizes)
-        rpn_anchor_generator = AnchorGenerator(anchor_sizes, aspect_ratios)
-        rpn_head = RPNHead(256, rpn_anchor_generator.num_anchors_per_location()[0])
-        model.rpn.anchor_generator = rpn_anchor_generator
-        model.rpn.head = rpn_head
+        # anchor_sizes = (
+        #                    (
+        #                        16,
+        #                        32,
+        #                        64,
+        #                        96,
+        #                        128,
+        #                    ),
+        #                ) * 3
+        # aspect_ratios = ((0.75, 1.0, 1.25),) * len(anchor_sizes)
+        # rpn_anchor_generator = AnchorGenerator(anchor_sizes, aspect_ratios)
+        # rpn_head = RPNHead(256, rpn_anchor_generator.num_anchors_per_location()[0])
+        # model.rpn.anchor_generator = rpn_anchor_generator
+        # model.rpn.head = rpn_head
         in_features = model.roi_heads.box_predictor.cls_score.in_features
         all_classes = n_classes + 1     # +1 class for background
         model.roi_heads.box_predictor = FastRCNNPredictor(in_features, all_classes)
