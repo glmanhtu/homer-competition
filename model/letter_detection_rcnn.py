f2563b5cb5c9782a6a9289a9d943656cd81d47ff
Manh Tu VU <glmanhtu@gmail.com>
2023-03-16 19:45:38 +0100

    Test restnet50

--- model/letter_detection_rcnn.py
+++ model/letter_detection_rcnn.py
@@ -9,7 +9,7 @@ class LetterDetectionRCNN(nn.Module):
 
     def __init__(self, device, n_classes, img_size, dropout=0.5):
         super().__init__()
-        model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True,
+        model = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(pretrained=True,
                                                                                min_size=img_size,
                                                                                max_size=img_size,
                                                                                rpn_batch_size_per_image=512,
@@ -18,20 +18,20 @@ class LetterDetectionRCNN(nn.Module):
                                                                                # box_score_thresh=0.3,
                                                                                box_detections_per_img=320)
 
-        anchor_sizes = (
-                           (
-                               32,
-                               64,
-                               96,
-                               128,
-                               160,
-                           ),
-                       ) * 3
-        aspect_ratios = ((0.5, 1.0, 2.0),) * len(anchor_sizes)
-        rpn_anchor_generator = AnchorGenerator(anchor_sizes, aspect_ratios)
-        rpn_head = RPNHead(256, rpn_anchor_generator.num_anchors_per_location()[0])
-        model.rpn.anchor_generator = rpn_anchor_generator
-        model.rpn.head = rpn_head
+        # anchor_sizes = (
+        #                    (
+        #                        32,
+        #                        64,
+        #                        96,
+        #                        128,
+        #                        160,
+        #                    ),
+        #                ) * 3
+        # aspect_ratios = ((0.5, 1.0, 2.0),) * len(anchor_sizes)
+        # rpn_anchor_generator = AnchorGenerator(anchor_sizes, aspect_ratios)
+        # rpn_head = RPNHead(256, rpn_anchor_generator.num_anchors_per_location()[0])
+        # model.rpn.anchor_generator = rpn_anchor_generator
+        # model.rpn.head = rpn_head
         in_features = model.roi_heads.box_predictor.cls_score.in_features
         all_classes = n_classes + 1     # +1 class for background
         model.roi_heads.box_predictor = FastRCNNPredictor(in_features, all_classes)
