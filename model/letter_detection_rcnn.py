fa2562a26fa14b67d7c05c3062c925ac8dd49b93
Manh Tu VU <glmanhtu@gmail.com>
2023-03-18 15:16:05 +0100

    Update net

--- model/letter_detection_rcnn.py
+++ model/letter_detection_rcnn.py
@@ -1,6 +1,7 @@
 import torchvision
 from torch import nn
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, FastRCNNConvFCHead, fasterrcnn_resnet50_fpn_v2
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, FastRCNNConvFCHead, fasterrcnn_resnet50_fpn_v2, \
+    fasterrcnn_mobilenet_v3_large_fpn
 from torchvision.ops import MultiScaleRoIAlign
 
 
@@ -16,6 +17,13 @@ class LetterDetectionRCNN(nn.Module):
                                                box_nms_thresh=0.3, box_score_thresh=0.3,
                                                box_fg_iou_thresh=0.75, box_bg_iou_thresh=0.5,
                                                box_detections_per_img=320)
+        elif arch == 'mobinet':
+            model = fasterrcnn_mobilenet_v3_large_fpn(pretrained=True, min_size=img_size, trainable_backbone_layers=6,
+                                                      max_size=img_size, rpn_batch_size_per_image=256,
+                                                      box_batch_size_per_image=512,
+                                                      box_nms_thresh=0.3, box_score_thresh=0.3,
+                                                      box_fg_iou_thresh=0.75, box_bg_iou_thresh=0.5,
+                                                      box_detections_per_img=320)
         else:
             raise Exception(f'Arch {arch} is not implemented')
 
