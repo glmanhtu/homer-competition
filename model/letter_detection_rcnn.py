5bae58e59071470fa76bde5c1fa25854912902c4
Manh Tu VU <mvu@mvu.labri.fr>
2023-03-20 10:52:13 +0100

    Update

--- model/letter_detection_rcnn.py
+++ model/letter_detection_rcnn.py
@@ -14,14 +14,12 @@ class LetterDetectionRCNN(nn.Module):
             model = fasterrcnn_resnet50_fpn_v2(pretrained=True, min_size=img_size, trainable_backbone_layers=5,
                                                max_size=img_size, rpn_batch_size_per_image=256,
                                                box_batch_size_per_image=512,
-                                               box_nms_thresh=0.3, box_score_thresh=0.3,
                                                box_fg_iou_thresh=0.75, box_bg_iou_thresh=0.5,
                                                box_detections_per_img=320)
         elif arch == 'mobinet':
             model = fasterrcnn_mobilenet_v3_large_fpn(pretrained=True, min_size=img_size, trainable_backbone_layers=6,
                                                       max_size=img_size, rpn_batch_size_per_image=256,
                                                       box_batch_size_per_image=512,
-                                                      box_nms_thresh=0.3, box_score_thresh=0.3,
                                                       box_fg_iou_thresh=0.75, box_bg_iou_thresh=0.5,
                                                       box_detections_per_img=320)
         else:
