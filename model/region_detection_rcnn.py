664c88f33affd5a37cc0ed9740ca9a08aa4e41e7
Manh Tu VU <glmanhtu@gmail.com>
2023-03-27 11:24:55 +0200

    Priotize region boxes

--- model/region_detection_rcnn.py
+++ model/region_detection_rcnn.py
@@ -10,6 +10,78 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, fasterrc
     fasterrcnn_resnet50_fpn_v2
 
 
+class BalancedPositiveNegativeSampler:
+    """
+    This class samples batches, ensuring that they contain a fixed proportion of positives
+    """
+
+    def __init__(self, batch_size_per_image: int, positive_fraction: float) -> None:
+        """
+        Args:
+            batch_size_per_image (int): number of elements to be selected per image
+            positive_fraction (float): percentage of positive elements per batch
+        """
+        self.batch_size_per_image = batch_size_per_image
+        self.positive_fraction = positive_fraction
+
+    def __call__(self, matched_idxs: List[Tensor]) -> Tuple[List[Tensor], List[Tensor]]:
+        """
+        Args:
+            matched idxs: list of tensors containing -1, 0 or positive values.
+                Each tensor corresponds to a specific image.
+                -1 values are ignored, 0 are considered as negatives and > 0 as
+                positives.
+
+        Returns:
+            pos_idx (list[tensor])
+            neg_idx (list[tensor])
+
+        Returns two lists of binary masks for each image.
+        The first list contains the positive elements that were selected,
+        and the second list the negative example.
+        """
+        pos_idx = []
+        neg_idx = []
+        for matched_idxs_per_image in matched_idxs:
+            positive = torch.where(matched_idxs_per_image >= 1)[0]
+            negative = torch.where(matched_idxs_per_image == 0)[0]
+
+            num_pos = int(self.batch_size_per_image * self.positive_fraction)
+            # protect against not enough positive examples
+            num_pos = min(positive.numel(), num_pos)
+            num_neg = self.batch_size_per_image - num_pos
+            # protect against not enough negative examples
+            num_neg = min(negative.numel(), num_neg)
+
+            num_pos_region = num_pos // 2
+            positive_region = torch.where(matched_idxs_per_image == 2)[0]
+            perm_pos_region = torch.randperm(positive_region.numel(), device=positive.device)[:num_pos_region]
+
+            num_pos_boxes = num_pos - len(perm_pos_region)
+            positive_region = torch.where(matched_idxs_per_image == 1)[0]
+            perm_pos_boxes = torch.randperm(positive_region.numel(), device=positive.device)[:num_pos_boxes]
+
+            # randomly select positive and negative examples
+            perm1 = torch.cat([perm_pos_region, perm_pos_boxes], dim=0)
+            perm2 = torch.randperm(negative.numel(), device=negative.device)[:num_neg]
+
+            pos_idx_per_image = positive[perm1]
+            neg_idx_per_image = negative[perm2]
+
+            # create binary mask from indices
+            pos_idx_per_image_mask = torch.zeros_like(matched_idxs_per_image, dtype=torch.uint8)
+            neg_idx_per_image_mask = torch.zeros_like(matched_idxs_per_image, dtype=torch.uint8)
+
+            pos_idx_per_image_mask[pos_idx_per_image] = 1
+            neg_idx_per_image_mask[neg_idx_per_image] = 1
+
+            pos_idx.append(pos_idx_per_image_mask)
+            neg_idx.append(neg_idx_per_image_mask)
+
+        return pos_idx, neg_idx
+
+
+
 def custom_post_process(
     self,
         class_logits,  # type: Tensor
@@ -98,6 +170,14 @@ class RegionDetectionRCNN(nn.Module):
             custom_post_process(model.roi_heads, class_logits, box_regression, proposals, image_shapes)
         in_features = model.roi_heads.box_predictor.cls_score.in_features
         model.roi_heads.box_predictor = FastRCNNPredictor(in_features, n_classes)
+        model.rpn.fg_bg_sampler = BalancedPositiveNegativeSampler(
+            model.rpn.fg_bg_sampler.batch_size_per_image,
+            model.rpn.fg_bg_sampler.positive_fraction
+        )
+        model.roi_heads.fg_bg_sampler = BalancedPositiveNegativeSampler(
+            model.roi_heads.fg_bg_sampler.batch_size_per_image,
+            model.roi_heads.fg_bg_sampler.positive_fraction
+        )
 
         self.network = model
         self.to(device)
