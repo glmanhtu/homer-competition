e35b79343fa25de355cbb15170b18bd1f0076ceb
VU Manh Tu <glmanhtu@gmail.com>
2023-04-01 07:28:31 +0200

    Add Resnet Custom

--- model/second_twin_rcnn.py
+++ model/second_twin_rcnn.py
@@ -1,6 +1,20 @@
 from torch import nn
+from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models.detection.anchor_utils import AnchorGenerator
+from torchvision.models.detection.backbone_utils import _resnet_fpn_extractor
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, fasterrcnn_resnet50_fpn_v2, \
-    fasterrcnn_mobilenet_v3_large_fpn
+    fasterrcnn_mobilenet_v3_large_fpn, FastRCNNConvFCHead, FasterRCNN
+from torchvision.models.detection.rpn import RPNHead
+from torchvision.ops.feature_pyramid_network import ExtraFPNBlock
+
+
+class NoExtraBlock(ExtraFPNBlock):
+    """
+    Applies a max_pool2d on top of the last feature map
+    """
+
+    def forward(self, x, y, names):
+        return x, names
 
 
 class SecondTwinRCNN(nn.Module):
@@ -15,6 +29,31 @@ class SecondTwinRCNN(nn.Module):
                                                box_fg_iou_thresh=0.75, box_bg_iou_thresh=0.5,
                                                box_positive_fraction=0.4,
                                                box_detections_per_img=320)
+        elif arch == 'resnet_custom':
+            backbone = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1, progress=True)
+            backbone = _resnet_fpn_extractor(backbone, 5, returned_layers=[2],
+                                             norm_layer=nn.BatchNorm2d, extra_blocks=NoExtraBlock())
+            anchor_sizes = ((64, 96, 128,),)
+            aspect_ratios = ((0.5, 1.0, 2.0),) * len(anchor_sizes)
+            rpn_anchor_generator = AnchorGenerator(anchor_sizes, aspect_ratios)
+            rpn_head = RPNHead(backbone.out_channels, rpn_anchor_generator.num_anchors_per_location()[0], conv_depth=2)
+            box_head = FastRCNNConvFCHead(
+                (backbone.out_channels, 7, 7), [256, 256, 256, 256], [1024], norm_layer=nn.BatchNorm2d
+            )
+            model = FasterRCNN(
+                backbone,
+                num_classes=n_classes + 1,
+                rpn_anchor_generator=rpn_anchor_generator,
+                rpn_head=rpn_head,
+                box_head=box_head,
+                min_size=img_size,
+                max_size=img_size, rpn_batch_size_per_image=256,
+                box_batch_size_per_image=512,
+                box_nms_thresh=0.5, box_score_thresh=0.2,
+                box_positive_fraction=0.4,
+                box_fg_iou_thresh=0.75, box_bg_iou_thresh=0.5,
+                box_detections_per_img=320
+            )
         elif arch == 'mobinet':
             model = fasterrcnn_mobilenet_v3_large_fpn(pretrained=True, min_size=img_size, trainable_backbone_layers=6,
                                                       max_size=img_size, rpn_batch_size_per_image=256,
