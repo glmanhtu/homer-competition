3d7612a61eaa8627c554096437d5462435862652
VU Manh Tu <glmanhtu@gmail.com>
2023-04-16 23:02:15 +0200

    Update

--- model/first_twin_rcnn.py
+++ model/first_twin_rcnn.py
@@ -10,10 +10,12 @@ class FirstTwinRCNN(nn.Module):
         if arch == 'resnet50':
             model = fasterrcnn_resnet50_fpn_v2(pretrained=True, min_size=img_size, trainable_backbone_layers=5,
                                                max_size=img_size,
+                                               box_score_thresh=0.2,
                                                box_detections_per_img=320)
         elif arch == 'mobinet':
             model = fasterrcnn_mobilenet_v3_large_fpn(pretrained=True, min_size=img_size, trainable_backbone_layers=6,
                                                       max_size=img_size,
+                                                      box_score_thresh=0.2,
                                                       box_detections_per_img=320)
         else:
             raise Exception(f'Arch {arch} is not implemented')
