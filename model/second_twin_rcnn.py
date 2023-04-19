030461cb3b2be40adbb7b5552b142cf449ed67da
VU Manh Tu <glmanhtu@gmail.com>
2023-04-19 14:04:01 +0200

    Update

--- model/second_twin_rcnn.py
+++ model/second_twin_rcnn.py
@@ -25,7 +25,7 @@ class SecondTwinRCNN(nn.Module):
             model = fasterrcnn_resnet50_fpn_v2(pretrained=True, min_size=img_size, trainable_backbone_layers=5,
                                                max_size=img_size, rpn_batch_size_per_image=256,
                                                box_batch_size_per_image=512,
-                                               box_nms_thresh=0.5, box_score_thresh=0.2,
+                                               box_nms_thresh=0.5, box_score_thresh=0.05,
                                                box_fg_iou_thresh=0.65, box_bg_iou_thresh=0.5,
                                                box_positive_fraction=0.3,
                                                box_detections_per_img=1000)
