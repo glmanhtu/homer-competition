c803b06ca803c972082315ce46bc94ef7941c4f9
VU Manh Tu <glmanhtu@gmail.com>
2023-04-19 18:58:51 +0200

    Update

--- dataset/dataset_factory.py
+++ dataset/dataset_factory.py
@@ -10,6 +10,7 @@ def get_dataset(dataset_path: str, mode, is_training, image_size_p1, image_size_
                               transforms=transforms, fold=fold, k_fold=k_fold, all_data=all_data)
     elif mode == 'second_twin':
         return PapyrusP2Dataset(dataset_path, is_training=is_training, image_size=image_size_p2,
-                                ref_box_size=ref_box_size, transforms=transforms, fold=fold, k_fold=k_fold)
+                                ref_box_size=ref_box_size, transforms=transforms, fold=fold, k_fold=k_fold,
+                                all_data=all_data)
     else:
         return PapyrusValDataset(dataset_path, is_training, fold=fold, k_fold=k_fold, all_data=all_data)
